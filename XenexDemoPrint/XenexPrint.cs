b72727b488eac4b70ef3fffb40cb26efbfdaed3f
diazg77 <gerojd@yahoo.com>
2017-03-22 11:33:00 -0500

    First Commit

---
+++ XenexDemoPrint/XenexPrint.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XenexDemoPrint
+{
+    public static class XenexPrint
+    {
+        public static IEnumerable<string> GenerateMessages(int loops, Dictionary<int, string> messageCases)
+        {
+            for (int i = 1; i <= loops; i++)
+            {
+                StringBuilder messageBuilder = new StringBuilder();
+                messageBuilder.Append(i.ToString());
+
+                foreach(KeyValuePair<int, string> messageCase in messageCases)
+                {
+                    if (i % messageCase.Key == 0)
+                        messageBuilder.Append(" " + messageCase.Value);
+                }
+
+                yield return messageBuilder.ToString();
+            }
+        }
+    }
+}
---
+++ XenexDemoTest/XenexPrintTest.cs
@@ -0,0 +1,70 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Text;
+using XenexDemoPrint;
+
+namespace XenexDemoTest
+{
+    [TestFixture]
+    public class XenexPrintTest
+    {
+        private Dictionary<int, string> messageCases = new Dictionary<int, string>();
+        private int TestLoopCount;
+
+        [OneTimeSetUp]
+        public void TestSetup()
+        {
+            messageCases.Add(3, "fizz");
+            messageCases.Add(5, "buzz");
+
+            TestLoopCount = 15;
+        }
+        [Test]
+        public void XenexPrint15MessageTest()
+        {            
+            var testMessage = "";            
+
+            foreach (string message in XenexPrint.GenerateMessages(TestLoopCount, messageCases))
+                testMessage = message;
+
+            Assert.AreEqual(testMessage, "15 fizz buzz");
+        }
+
+        [Test]
+        public void XenexPrintLoopCountTest()
+        {
+            int loopCount = 0;
+
+            foreach (string message in XenexPrint.GenerateMessages(TestLoopCount, messageCases))
+                loopCount++;
+
+            Assert.AreEqual(TestLoopCount, loopCount);
+        }
+
+        [Test]
+        public void XenexPrintExpectedMessageTest()
+        {
+            var messages = new List<string>();
+            for (int i = 1; i <= TestLoopCount; i++)
+            {
+                StringBuilder messageBuilder = new StringBuilder();
+                messageBuilder.Append(i.ToString());
+
+                foreach (KeyValuePair<int, string> messageCase in messageCases)
+                {
+                    if (i % messageCase.Key == 0)
+                        messageBuilder.Append(" " + messageCase.Value);
+                }
+
+                messages.Add(messageBuilder.ToString());
+            }
+
+            int index = 0;
+            foreach (string message in XenexPrint.GenerateMessages(TestLoopCount, messageCases))
+            {                
+                Assert.AreEqual(message, messages[index]);
+                index++;
+            }
+        }
+    }
+}
